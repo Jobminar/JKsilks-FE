d1693bed8c37777141eb3923c8f1c09227331839
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2023-12-07 16:47:36 +0530

    sameer today 16:47

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
-import { Routes,Route , BrowserRouter } from "react-router-dom"
-import Footer from "./components/Footer/footer"
-import Navbar from "./components/Navbar/Navbar"
-import Home from "./components/Home/Home"
-
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer/footer";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Login from "./components/Profile/Login";
+import Signup from "./components/Profile/Signup";
+import ShoppingCart from "./components/Cart/ShoppingCart";
+import Account from "./components/Profile/Account";
+import Favourites from "./components/Favourites/Favourites";
+import AddTocart from "./components/Cart/AddTocart";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-       <Navbar/>
+        <Navbar />
         <Routes>
-           <Route path='/' element={<Home/>}/>
-             
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/cart" element={<ShoppingCart />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/favourite" element={<Favourites />} />
+          <Route path="/addtocart" element={<AddTocart />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
-       
-       
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,49 @@
-import './Navbar.css'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Navbar.css";
+import logo from "./images/logo.png";
+import cart from "./images/cart.png";
+import search from "./images/search.png";
+import wishlist from "./images/wishlist.png";
+import profile from "./images/profile.png";
 
-// images
-import logo from './images/logo.png'
-import cart from './images/cart.png'
-import serch from './images/serch.png'
-import wishlist from './images/wishlist.png'
-import profile from './images/profile.png'
+const Navbar = () => {
+  const navigate = useNavigate();
 
-const Navbar = (()=>{
-    return(
-        <>
-              <div className="navbar-con">
-                <img src={logo} alt='logo'/>
-                <p>Semi Kanchi pattu</p>
-                <p>Light weight pattu</p>
-                <p>Soft Silk</p>
-                <p>Pure Kanchi Pattu</p>
-                <p>Exclusive Bridal wear</p>
-                <p>Pure Kanjivaram Silk</p>
-                <img src={serch} alt='serch'/>
-                <img src={wishlist} alt='wishlist'/>
-                <img src={cart} alt='cart'/>
-                <img src={profile} alt='profile'/>
+  const handleHomeClick = () => {
+    navigate("/");
+  };
+  const handleSearchClick = () => {
+    navigate("/search");
+  };
 
-              </div>
-        </>
-    )
-})
-export default Navbar
+  const handleWishlistClick = () => {
+    navigate("/favourite");
+  };
+
+  const handleCartClick = () => {
+    navigate("/cart");
+  };
+
+  const handleProfileClick = () => {
+    navigate("/account");
+  };
+
+  return (
+    <div className="navbar-con">
+      <img src={logo} alt="logo" onClick={handleHomeClick} />
+      <p>Semi Kanchi pattu</p>
+      <p>Light weight pattu</p>
+      <p>Soft Silk</p>
+      <p>Pure Kanchi Pattu</p>
+      <p>Exclusive Bridal wear</p>
+      <p>Pure Kanjivaram Silk</p>
+      <img src={search} alt="search" onClick={handleSearchClick} />
+      <img src={wishlist} alt="wishlist" onClick={handleWishlistClick} />
+      <img src={cart} alt="cart" onClick={handleCartClick} />
+      <img src={profile} alt="profile" onClick={handleProfileClick} />
+    </div>
+  );
+};
+
+export default Navbar;
